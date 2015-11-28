c0cf66a4392e9bb833a94f94da482e180b903dcf
andreahaku <andreahaku@gmail.com>
2015-11-28 16:25:31 +0100

    aggiunti servizio encoding dell'URL e servizio date

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -53,11 +53,14 @@ angular.module('stockDashbord.controllers', [])
     ];
 }])
 
-.controller('StockCtrl', [ '$scope', '$stateParams', 'stockDataService',
-  function($scope, $stateParams, stockDataService) {
+.controller('StockCtrl', [ '$scope', '$stateParams', 'stockDataService', 'dateService',
+  function($scope, $stateParams, stockDataService, dateService) {
 
   $scope.ticker = $stateParams.stockTicker;
   $scope.chartView = 1;
+
+console.log(dateService.currentDate());
+console.log(dateService.oneYearAgoDate());
 
   $scope.$on("$ionicView.afterEnter", function() {
     getPriceData();
