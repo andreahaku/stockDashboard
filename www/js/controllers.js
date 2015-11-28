30f9c9865b5e14a4e4c24372f5f361fa0a24291a
andreahaku <andreahaku@gmail.com>
2015-11-28 19:37:01 +0100

    aggiunta css styling delle chart

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -140,7 +140,7 @@ angular.module('stockDashbord.controllers', [])
   	$scope.chartOptions = {
       chartType: 'linePlusBarWithFocusChart',
       data: 'myData',
-      margin: {top: 15, right: 40, bottom: marginBottom, left: 70},
+      margin: {top: 15, right: 0, bottom: marginBottom, left: 0},
       interpolate: "cardinal",
       useInteractiveGuideline: false,
       yShowMaxMin: false,
@@ -155,7 +155,10 @@ angular.module('stockDashbord.controllers', [])
       y2AxisTickFormat: y2TickFormat,
       y3AxisTickFormat: y3TickFormat,
       y4AxisTickFormat: y4TickFormat,
-      transitionDuration: 500
+      transitionDuration: 500,
+      y1AxisLabel: 'Price',
+      y3AxisLabel: 'Volume',
+      noData: 'loading data...'
   	};
 
 }]);
