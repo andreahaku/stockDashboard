5601129d76d7f06d720c1ae391d3c1e1be344499
andreahaku <andreahaku@gmail.com>
2015-11-29 19:12:12 +0100

    aggiunte le note con relativo styling

--- www/js/services.js
+++ www/js/services.js
@@ -76,6 +76,24 @@ else {
 })
 
 
+.factory('notesCacheService', function(CacheFactory) {
+
+  var notesCache;
+
+  if(!CacheFactory.get('notesCache')){
+
+    notesCache = CacheFactory('notesCache', {
+      storageMode: 'localStorage'
+    });
+  }
+  else {
+    notesCache = CacheFactory.get('notesCache');
+  }
+
+  return notesCache;
+
+})
+
 
 .factory('stockDataService', function ($q, $http, encodeURIService, stockDetailsCacheService){
 
@@ -201,6 +219,42 @@ else {
     getHistoricalData: getHistoricalData
   };
 
+})
+
+.factory('notesService', function(notesCacheService){
+
+  return {
+
+    getNotes: function(ticker){
+      return notesCacheService.get(ticker);
+    },
+
+    addNotes: function(ticker, note){
+
+      var stockNotes = [];
+
+      if (notesCacheService.get(ticker)){
+
+        stockNotes = notesCacheService.get(ticker);
+        stockNotes.push(note);
+      }
+      else {
+        stockNotes.push(note);
+      }
+
+      notesCacheService.put(ticker, stockNotes);
+    },
+
+    deleteNotes: function(ticker, index){
+
+      var stockNotes = [];
+
+      stockNotes = notesCacheService.get(ticker);
+      stockNotes.splice(index,1);
+      notesCacheService.put(ticker, stockNotes);
+
+    }
+  };
 })
 
 ;
